1e2eff770c0b44aa59f238e01d87f6115f5c94f2
yethranayeh <aktasalper99@gmail.com>
2022-02-22 00:00:16 +0300

    form fields now empty after submit

--- src/AddBooks.tsx
+++ src/AddBooks.tsx
@@ -1,11 +1,25 @@
 /** @format */
 
 export default function AddBooks({ submitHandler }: { submitHandler: Function }) {
+	function submitForm(e: any) {
+		submitHandler(e);
+
+		// Empty all form input fields
+		const inputs = document.querySelectorAll("input:not([type='checkbox'])") as any as HTMLInputElement[];
+		inputs.forEach((input) => {
+			input.value = "";
+		});
+
+		// Uncheck checkbox
+		const checkbox = document.querySelector("input[type='checkbox']") as HTMLInputElement;
+		checkbox.checked = false;
+	}
+
 	return (
 		<div className='AddBooks'>
 			<div className='col'>
 				<h1>Add Books</h1>
-				<form onSubmit={(e) => submitHandler(e)}>
+				<form onSubmit={(e) => submitForm(e)}>
 					<div className='row input-field'>
 						<label htmlFor='bookName'>Book Name:</label>
 						<input type='text' id='bookName' name='bookName' required />
